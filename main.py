2dd967d411063e3d05176957e391dffded6b5a72
rishikeshnayar <rishikeshnayarsankar@gmail.com>
2023-06-09 22:09:09 -0700

    Added comments to main file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import send_sms
 import webscraper
 
+# Main function.  Uses condition functions to check if RSF is below or above threshold, sends appropriate SMS, and switches condition
 def main(threshold=40):
     conditions = [original_cond, secondary_cond]
     texts = [f"The RSF is less than {threshold}% full!  You should consider going :D", f"The RSF has exceeded the {threshold}% threshold - perhaps wait until it's less busy."]
@@ -13,11 +14,12 @@ def main(threshold=40):
             i += 1 # Change condition to new condition
         time.sleep(600) # Check crowdmeter again after 10 minutes
 
-
+# Function to check if RSF is below threshold
 def original_cond(threshold, curr_percent):
     if curr_percent < threshold:
         return True
 
+# Function to check if RSF is above threshold + 5
 def secondary_cond(threshold, curr_percent):
     if curr_percent >= threshold + 5:
         return True
